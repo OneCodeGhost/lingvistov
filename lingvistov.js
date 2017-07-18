52fd9faa9b3fbd9a412c4b4155c83315d4018caf
1 <1>
2017-07-18 15:55:52 +0800

    none

---
+++ lingvistov.js
@@ -0,0 +1,11 @@
+$(function(){
+  var li=$('.carousel-item-child')[0]; //轮播图的数量
+
+  $('.cbt').on('click',function(){
+    if($(this).data('control')==="prev"){
+      
+    }else{
+
+    }
+  })
+});
